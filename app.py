e59ef538a0ec38cfef07cd3a9eaa3d176b2ed6e1
enrique28-ai <enriqueprecia@gmail.com>
2025-04-03 02:02:19 -0700

    Post request

--- app.py
+++ app.py
@@ -12,10 +12,11 @@ def index_get():
 
 @app.route("/predict", methods=["GET", "POST"])
 def predict():
-    text = request.get_json().get("message")
-    response = get_response(text)
-    message = {"answer": response}
-    return jsonify(message)
+    if request.method == "POST":
+        text = request.get_json().get("message")
+        response = get_response(text)
+        message = {"answer": response}
+        return jsonify(message)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000,debug=True)
